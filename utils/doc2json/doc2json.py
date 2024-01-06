c32e5e336c81a9ebdf41aed68b4886597a6ff47f
Dvd848 <Dvd848@users.noreply.github.com>
2024-01-06 20:28:33 +0200

    doc2json: Add explicit solutions

--- utils/doc2json/doc2json.py
+++ utils/doc2json/doc2json.py
@@ -31,24 +31,27 @@ def process_docx_directory(directory_path, out_dir):
                 break
             seen.add(cid)
 
-    max_id = max(seen)
     missing = []
+    if (len(seen)) == 0:
+        return missing
+    
+    max_id = max(seen)
     for i in range(1, max_id + 1):
         if i not in seen:
             missing.append(i)
 
-    print("The following crosswords are missing: ", missing)
+    return missing
+
+class State(enum.Enum):
+    READ_CROSSWORD_ID = "id"
+    READ_AUTHOR = "author"
+    READ_DOWN_TITLE = "down_title"
+    READ_DOWN_DEFINITIONS = "down"
+    READ_ACROSS_TITLE = "across_title"
+    READ_ACROSS_DEFINITIONS = "across"
 
 def process_crossword(path, out_dir):
     document = Document(path)
-
-    class State(enum.Enum):
-        READ_CROSSWORD_ID = "id"
-        READ_AUTHOR = "author"
-        READ_DOWN_TITLE = "down_title"
-        READ_DOWN_DEFINITIONS = "down"
-        READ_ACROSS_TITLE = "across_title"
-        READ_ACROSS_DEFINITIONS = "across"
 
     crossword = {
         State.READ_CROSSWORD_ID.value: 0,
@@ -121,17 +124,63 @@ def process_crossword(path, out_dir):
     if has_sol:
         crossword["sol_hash"] = hashlib.sha512("".join([item for sublist in grid_sol for item in sublist]).encode()).hexdigest()
         crossword["sol_grid"] = grid_sol
-
+        try:
+            crossword["solutions"] = extract_definition_solutions(crossword)
+        except Exception as e:
+            print(f"Error extracting definition solutions for crossword {crossword['id']}")
+            print("This usually happens if the solution isn't complete.")
+            print("For example: A missing character in the solution.")
 
     with open(os.path.join(out_dir, f"{crossword[State.READ_CROSSWORD_ID.value]}.json"), "w", encoding="utf8") as o:
         json.dump(crossword, o, indent=2)
 
     return crossword[State.READ_CROSSWORD_ID.value]
 
+def get_def_sol(row, col, solution, progress):
+    res = []
+    dr, dc = progress
+
+    def in_bounds(row, col):
+        if row < 0 or row >= len(solution):
+            return False
+        if col < 0 or col >= len(solution[0]):
+            return False
+        return True
+
+    while in_bounds(row, col) and (solution[row][col] != '#'):
+        res.append(solution[row][col])
+        row += dr
+        col += dc
+    
+    return "".join(res)
+
+def extract_definition_solutions(crossword_data):
+    res = {}
+
+    directions = {
+        State.READ_DOWN_DEFINITIONS.value:   (1, 0), 
+        State.READ_ACROSS_DEFINITIONS.value: (0, -1)
+    }
+
+    for k in directions:
+        res[k] = {}
+
+    for r in range(crossword_data["dimensions"]["rows"]):
+        for c in range(crossword_data["dimensions"]["columns"]):
+            if (crossword_data["grid"][r][c].isnumeric()):
+                definition_number = crossword_data["grid"][r][c]
+                for direction, progress in directions.items():
+                    if int(definition_number) in crossword_data["definitions"][direction]:
+                        res[direction][definition_number] = get_def_sol(r, c, 
+                                                                        crossword_data["sol_grid"], 
+                                                                        progress)
+    return res
+
 def main():
     parser = argparse.ArgumentParser(description="Convert Cryptic Crossword from docx to json")
     parser.add_argument("-i", "--input_path", required=True, help="Path to docx file or directory containing docx files")
     parser.add_argument("-o", "--out_dir", required=True, help="Output directory")
+    parser.add_argument("-s", "--show_missing", action='store_true', help="List missing crosswords when parsing directory")
     args = parser.parse_args()
 
     input_path = args.input_path
@@ -140,7 +189,9 @@ def main():
     if os.path.isfile(input_path):
         process_crossword(input_path, out_dir)
     elif os.path.isdir(input_path):
-        process_docx_directory(input_path, out_dir)
+        missing = process_docx_directory(input_path, out_dir)
+        if args.show_missing:
+            print("The following crosswords are missing: ", missing)
     else:
         print("Invalid input path.")
 
